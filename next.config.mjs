315472687747129636845674f83450963bbf67d3
kijima-riku <riku.bulls.0116@icloud.com>
2024-09-24 18:46:19 +0900

    library , image settings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pwsironzaezqdvhbqdlq.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+    loader: "custom",
+    loaderFile: "./src/libs/imageLoader.js",
+  },
+};
 
 export default nextConfig;
